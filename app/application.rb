aa093c7f82640578a0794da48458f968cc8adebe
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-24 02:32:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,18 @@
 class Application
   def call(env)
+    @@item =[]
       resp = Rack::Response.new
       req = Rack::Request.new(env)
 
 
-      if req.path == "/items"
-        resp.write "You requested the #{item.price}"
-
+      if req.path.match(/items/)
+        dynamic =reg.path.split("/items/").last
+        @@item.find do |i|i.name == dynamic
+        resp.write "You requested the #{i.price}"
+      end
+    else
+      resp.status = 400
+      resp.write "Item not found"
       else
         resp.status = 404
         resp.write "Route not found"
